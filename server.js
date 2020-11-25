d976a2e7339c0f33e1c88f0379d27e8e6ccca1b2
Inusah-Said <saidinusah29@gmail.com>
2020-11-25 11:45:02 +0000

    b.e send and retrieve message done

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //importing stuff
 import express from 'express';
 import mongoose from 'mongoose';
+import Messages from './dbMessages.js';
 //app configs
 const app = express();
 const port = process.env.PORT || 9000;
 //middleware
-
+app.use(express.json());
 //database config
 const connectionURL =
 	'mongodb+srv://admin:MVLc3PFMhzEefMcP@theghetto.umr7e.mongodb.net/whatsappDB?retryWrites=true&w=majority';
@@ -20,7 +21,22 @@ mongoose
 
 //??
 
-//api routes
-app.get('/', (req, res) => res.status(200).send('hello world'));
+//api routes//
+app.get('/api/', (req, res) => res.status(200).send('hello world'));
+
+// creating the message in the db
+app.post('/api/messages/new', (req, res) => {
+	const dbMessage = req.body;
+	Messages.create(dbMessage, (err, data) => {
+		err ? res.status(500).send(err) : res.status(201).send(data);
+	});
+});
+// route to return all the messages or items in the db
+app.get('/api/messages/sync', (req, res) => {
+	// i used status code 200 since we aren't creating a new item but only finding
+	Messages.find((err, data) =>
+		err ? res.status(500).send(err) : res.status(200).send(data)
+	);
+});
 //listenting
 app.listen(port, () => console.log(`listening on port ${port}`));
